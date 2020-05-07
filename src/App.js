a9944c14f1ed1e5511c2623630b6f28ce57e2fa0
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-07 01:39:00 +0100

    newfiles

--- src/App.js
+++ src/App.js
@@ -27,8 +27,9 @@ function App() {
           <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp" alt="Las Vegas" />
           <p className="legend">Las Vegas</p>
         </div>
-      <Carousel />  
-    </div>    
+      </Carousel>  
+    </div> 
+    );  
 }
 
 
