ece5cae9983508313789e74bac8049ecc0b48f37
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-07 01:31:23 +0100

    newfiles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
+let styles = {
+  margin: 'auto',
+  width: '500px'
+};
+
 
 
 
