6e9f320da0395fe841f686f25b8a4377ea6fba36
Adedipupo <bayonleolaluwa@gmail.com>
2020-05-07 01:37:41 +0100

    newfiles

--- src/App.js
+++ src/App.js
@@ -15,6 +15,10 @@ function App() {
           <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg" alt="Hong Kong" />
           <p className="legend">Hong Kong</p>
         </div>
+        <div>
+          <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp" alt="Singapore" />
+          <p className="legend"></p>
+        </div>
       <Carousel />  
     </div>    
 }
